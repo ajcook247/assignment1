da33140d9e776b6f693f9686e84cb38c4dfe6eab
ajcook247 <43277610+ajcook247@users.noreply.github.com>
2019-08-28 19:30:13 -0400

    commit

---
+++ src/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+// Adam Cook
+// COP 3330
+// Assignment #1
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
